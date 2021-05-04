b7973d333da7d8f3489de4b534d0a908a19619a3
avoajaugochukwu <karlthegoo@hotmail.com>
2021-05-04 20:53:05 +0100

    Added Logger

--- cumba_bikes/settings.py
+++ cumba_bikes/settings.py
@@ -63,6 +63,41 @@ else:
     
     # print(DEV_ENV)
     # print('-------------- PRODDUCTION ENVIRONMENT ----------')
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                        'pathname=%(pathname)s lineno=%(lineno)s ' +
+                        'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'testlogger': {
+                'handlers': ['console'],
+                'level': 'INFO',
+            }
+        }
+    }
+    import logging
+    logger = logging.getLogger('testlogger')
+    logger.info('This is a simple log message')
 
 
 # Application definition
