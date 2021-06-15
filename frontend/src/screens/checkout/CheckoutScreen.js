3a192bce5c51c804ca44d2c99658c36d70c0fdb1
avoajaugochukwu <karlthegoo@hotmail.com>
2021-06-15 15:03:53 +0100

    Removed cmments from checkout page

--- frontend/src/screens/checkout/CheckoutScreen.js
+++ frontend/src/screens/checkout/CheckoutScreen.js
@@ -18,121 +18,93 @@ const { Title } = Typography;
 
 const CheckoutScreen = () => {
 
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart
-    const totalPrice = getCartTotalAmount(cartItems)
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart
+  const totalPrice = getCartTotalAmount(cartItems)
 
-    console.log(cartItems)
-    return (
-        <>
+  console.log(cartItems)
+  return (
+    <>
+      <Row justify="center" gutter={16} style={{ margin: '1px' }}>
+          <div>
+          <Title style={{ textAlign: "left", margin: "20px" }}>Order Details</Title>
+        </div>        
+      </Row>
 
+      <Row justify="center" gutter={16} style={{ margin: '1px' }}>
+        <Col xs={24} sm={12} md={12} lg={10}>
+          <div>
+            <AddressWrapper />
+          </div>
+          {cartItems.length > 0 ? cartItems.map(product => (
+            <div className="CheckOutScreen-Product-Wrapper" key={product.id}>
+              <div className="CheckOutScreen-Product CheckOutScreen-Product-Box1">
 
-            <Row justify="center" gutter={16} style={{ margin: '1px' }}>
-                {/* <Col xs={24} sm={12} md={12} lg={15}> */}
-                    {/* <p style={{ marginBottom: "10px" }}>{numberOfItemsInCart} item(s) in your shopping cart</p> */}
-                    <div>
-                        <Title style={{ textAlign: "left", margin: "20px"}}>Order Details</Title>
-                    </div>
-                    
-                {/* </Col> */}
-                {/* <Col xs={24} sm={12} md={8} lg={6}> */}
-                    {/* . */}
-                {/* </Col> */}
-            </Row>
-
-            
-            <Row justify="center" gutter={16} style={{ margin: '1px' }}>
-                <Col xs={24} sm={12} md={12} lg={10}>
-                    <div>
-                        <AddressWrapper />
-                    </div>
-                    {cartItems.length > 0 ? cartItems.map(product => (
-                        <div className="CheckOutScreen-Product-Wrapper" key={product.id}>
-                            <div className="CheckOutScreen-Product CheckOutScreen-Product-Box1">
-                                {/* <img src={product.images && product.images[1].image.full_size} width="10%" /> */}
-                                <Avatar
-                                    shape="square"
-                                    // size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-                                    size={64}
-                                    src={product.images && product.images[0].image_url} />
-                            </div>
-                            <div className="CheckOutScreen-Product CheckOutScreen-Product-Box2">
-                                <h4>{product.name}</h4>
-                                {/* <p>{product.description}</p> */}
-                                {/* <p><i>Brought to you by: {product.seller.company_name}</i></p> */}
-                            </div>
-                            <div className="CheckOutScreen-Product CheckOutScreen-Product-Box3">
-                                ${getTwoDecimalOfPrice(product.price)} <TagsOutlined />
-                            </div>
-                            <div className="CheckOutScreen-Product CheckOutScreen-Product-Box4">
-
-                                {/* <RestOutlined style={{color: "#ff4d4f", fontSize: '1.5em'}} /> */}
-                                {/* <Button type="link" danger onClick={() => handleRemoveItemFromCart(product.id)}>
-                                    Remove
-                                </Button> */}
-                            </div>
-                        </div>
-                    ))
-                        :
-                        <h1 style={{ marginTop: "30px" }}>No item in your cart, please visit our product page</h1>}
-
-                </Col>
-                <Col xs={24} sm={12} md={8} lg={6}>
-                    <div className="CheckOutScreen-Summary-Wrapper">
-                        <Link to="/cart/">
-                            Back to cart?
-                        </Link>
-                        <Title level={1}>Summary</Title>
-
-                        {/* <Title level={1}>${totalPrice}</Title> */}
-                        <div className="">
-                            <div className="CheckOutScreen-Summary-Amount">
-                                <p>Original price:</p>
-                                <p>${totalPrice}</p>
-                            </div>
-                            <div className="CheckOutScreen-Summary-Amount">
-                                <p>Coupon discounts:</p>
-                                <p>- &nbsp;$0.00</p>
-                            </div>
-                            <hr style={{ marginTop: "-5px", marginBottom: "5px" }} />
-                            <div className="CheckOutScreen-Summary-Amount">
-                                <h3>Total:</h3>
-                                <h3>${totalPrice}</h3>
-                            </div>
-                            <div>
-                                <p style={{ fontSize: "80%", marginTop: "20px", marginBottom: "20px" }}>
-                                    Shop is required by law to collect applicable transaction taxes for purchases made in certain tax jurisdictions.
-                                    By completing your purchase you agree to these <a>Terms of Service.</a>
-                                </p>
-                            </div>
-                        </div>
-                        {
-                            totalPrice > 0 ?
-                                <div>
-
-                                    {/* <Button type="primary" size="large" block style={{ marginBottom: "10px"}}>
-                                        <Link to="/checkout/">
-                                            Complete Payment
-                                        </Link>
-                                    </Button> */}
-                                    <PayPalButton />
-                                    {/* <Button size="large" block style={{ marginTop: "5px" }}> */}
-
-                                    {/* </Button> */}
-
-                                </div>
-                                :
-                                <Button type="dashed" block><Link to="/">Continue shopping</Link></Button>
-                        }
-                    </div>
-                </Col>
-            </Row>
-            <div style={{marginTop: "40px"}}>
-
+                <Avatar
+                  shape="square"
+                  size={64}
+                  src={product.images && product.images[0].image_url} />
+              </div>
+              <div className="CheckOutScreen-Product CheckOutScreen-Product-Box2">
+                <h4>{product.name}</h4>
+              </div>
+              <div className="CheckOutScreen-Product CheckOutScreen-Product-Box3">
+                ${getTwoDecimalOfPrice(product.price)} <TagsOutlined />
+              </div>
+              <div className="CheckOutScreen-Product CheckOutScreen-Product-Box4">
+              </div>
             </div>
-            <Footer  />
-        </>
-    )
+          ))
+            :
+            <h1 style={{ marginTop: "30px" }}>No item in your cart, please visit our product page</h1>}
+
+        </Col>
+        <Col xs={24} sm={12} md={8} lg={6}>
+          <div className="CheckOutScreen-Summary-Wrapper">
+            <Link to="/cart/">
+              Back to cart?
+                        </Link>
+            <Title level={1}>Summary</Title>
+
+          
+            <div className="">
+              <div className="CheckOutScreen-Summary-Amount">
+                <p>Original price:</p>
+                <p>${totalPrice}</p>
+              </div>
+              <div className="CheckOutScreen-Summary-Amount">
+                <p>Coupon discounts:</p>
+                <p>- &nbsp;$0.00</p>
+              </div>
+              <hr style={{ marginTop: "-5px", marginBottom: "5px" }} />
+              <div className="CheckOutScreen-Summary-Amount">
+                <h3>Total:</h3>
+                <h3>${totalPrice}</h3>
+              </div>
+              <div>
+                <p style={{ fontSize: "80%", marginTop: "20px", marginBottom: "20px" }}>
+                  Shop is required by law to collect applicable transaction taxes for purchases made in certain tax jurisdictions.
+                                    By completing your purchase you agree to these <a>Terms of Service.</a>
+                </p>
+              </div>
+            </div>
+            {
+              totalPrice > 0 ?
+                <div>
+                  <PayPalButton />
+                </div>
+                :
+                <Button type="dashed" block><Link to="/">Continue shopping</Link></Button>
+            }
+          </div>
+        </Col>
+      </Row>
+      <div style={{ marginTop: "40px" }}>
+
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default CheckoutScreen
